d3a60db7687ae36c8c56b1d9991f2268c37bdef1
Michael Rice <michael.rice@rackspace.com>
2014-07-15 22:10:32 -0500

    Added example to show how to check the vSphere api from perl via nagios or icinga

---
+++ check_vsphere.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+#
+# Simple script to run from icinga/nagios to monitor the API
+# of a vshpere host.
+#
+use warnings;
+use strict;
+use lib "/usr/lib64/nagios/plugins";
+use utils qw(%ERRORS);
+
+BEGIN { $ENV{PERL_LWP_SSL_VERIFY_HOSTNAME} = 0 }
+
+use VMware::VIRuntime;
+
+Opts::parse();
+Opts::validate();
+eval {
+    Util::connect();
+    my $service_instance = Vim::get_service_instance();
+    &set_time($service_instance->CurrentTime());
+    Util::disconnect();
+};
+
+if($@) {
+    print "API FAIL - error: / $!;\n";
+    exit  $ERRORS{'CRITICAL'};
+}
+
+my $time;
+sub set_time() {
+    $time = shift;
+}
+
+print "API OK - time: / $time;\n";
+exit $ERRORS{'OK'};
